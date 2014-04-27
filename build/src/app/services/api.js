d17a2a887e25a6c36e0b3ab66790937e0dbeefa1
Thien Pham <thienpham.us@gmail.com>
2014-04-27 15:04:53 -0700

    fix fonts location

--- build/src/app/services/api.js
+++ build/src/app/services/api.js
@@ -1,30 +1,21 @@
-angular.module('qpham.services.api', [])
-  .factory('apiFactory', function($http) {
+angular.module('qpham.services.api', []).factory('apiFactory', [
+  '$http',
+  function ($http) {
     var urlBase = '/api';
     var contacts = $http.get(urlBase + '/contacts').then(function (resp) {
-      return resp.data;
-    });
-
+        return resp.data;
+      });
     var apiFactory = {};
-
-    apiFactory.getStates = function() { 
-      /*return Restangular.all('states').getList();*/
+    apiFactory.getStates = function () {
       return $http.get(urlBase + '/states');
     };
-
-    apiFactory.getContacts = function() { 
-      /*return Restangular.all('contacts').getList();*/
-      /*return $http.get(urlBase + '/contacts');*/
+    apiFactory.getContacts = function () {
       return contacts;
     };
-
-    apiFactory.getContact = function(id) { 
-      /*return Restangular.all('contacts').getList();*/
+    apiFactory.getContact = function (id) {
       return $http.get(urlBase + '/contacts/' + id);
     };
-
     return apiFactory;
-
-  })
-
-  ;
+  }
+]);
+;
--- build/src/app/services/firerefservice.js
+++ build/src/app/services/firerefservice.js
@@ -1,17 +1,17 @@
-angular.module('qpham.services.firebaseRefs', ['firebase'])
-  .factory('FireRef', ['FBURL', '$firebase',
-    function(FBURL, $firebase) {
-      return {
-        projects: function() {
-          return $firebase(new Firebase(FBURL+'/projects'));
-        },
-        
-        contacts: function() {
-          return $firebase(new Firebase(FBURL+'/contacts'));
-        },
-        
-        states: function() {
-          return new Firebase(FBURL+'/states');
-        }
-      };
-    }]);
+angular.module('qpham.services.firebaseRefs', ['firebase']).factory('FireRef', [
+  'FBURL',
+  '$firebase',
+  function (FBURL, $firebase) {
+    return {
+      projects: function () {
+        return $firebase(new Firebase(FBURL + '/projects'));
+      },
+      contacts: function () {
+        return $firebase(new Firebase(FBURL + '/contacts'));
+      },
+      states: function () {
+        return new Firebase(FBURL + '/states');
+      }
+    };
+  }
+]);
