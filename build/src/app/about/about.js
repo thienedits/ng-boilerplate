5f04f597e43893abc1dfeb47628e87e855868070
Thien Pham <thienpham.us@gmail.com>
2014-06-20 16:14:43 -0700

    pagespeed insights tweaks

--- build/src/app/about/about.js
+++ build/src/app/about/about.js
@@ -1,30 +1,27 @@
-angular.module( 'qpham.about', [
-  'ui.router'
-])
-
-.config(function config( $stateProvider ) {
-  $stateProvider.state( 'about', {
-    url: '/about',
-    views: {
-      "main": {
-        controller: 'AboutCtrl',
-        templateUrl: 'about/about.tpl.html'
-      }
-    },
-    data:{ pageTitle: 'What is It?' }
-  });
-})
-
-.controller( 'AboutCtrl', function AboutCtrl( $scope ) {
-  $scope.loadingObj.loading = 'false';
-  $scope.expanded = false;
-
-  $scope.dropdownDemoItems = [
-    "The first choice!",
-    "And another choice for you.",
-    "but wait! A third!"
-  ];
-
-})
-
+angular.module('qpham.about', ['ui.router']).config([
+  '$stateProvider',
+  function config($stateProvider) {
+    $stateProvider.state('about', {
+      url: '/about',
+      views: {
+        'main': {
+          controller: 'AboutCtrl',
+          templateUrl: 'about/about.tpl.html'
+        }
+      },
+      data: { pageTitle: 'What is It?' }
+    });
+  }
+]).controller('AboutCtrl', [
+  '$scope',
+  function AboutCtrl($scope) {
+    $scope.loadingObj.loading = 'false';
+    $scope.expanded = false;
+    $scope.dropdownDemoItems = [
+      'The first choice!',
+      'And another choice for you.',
+      'but wait! A third!'
+    ];
+  }
+]);
 ;
--- build/src/app/contacts/contacts-directives.js
+++ build/src/app/contacts/contacts-directives.js
@@ -1,12 +1,6 @@
-angular.module('contacts.directives', [])
-  .directive('sortBy', function () {
-    return {
-    template: '<a ng-click="sort(sortvalue)"> ' +
-              '<span ng-transclude=""></span>' +
-              '<span ng-show="sortedby == sortvalue">' +
-              
-              '</span>' +
-              '</a>',
+angular.module('contacts.directives', []).directive('sortBy', function () {
+  return {
+    template: '<a ng-click="sort(sortvalue)"> ' + '<span ng-transclude=""></span>' + '<span ng-show="sortedby == sortvalue">' + '</span>' + '</a>',
     restrict: 'A',
     transclude: true,
     scope: {
@@ -27,36 +21,33 @@ angular.module('contacts.directives', [])
       };
     }
   };
-})
-
-.directive('onBlurChange', function ($parse) {
-  return function (scope, element, attr) {
-    var fn = $parse(attr['onBlurChange']);
-    var hasChanged = false;
-    element.on('change', function (event) {
-      hasChanged = true;
-    });
- 
-    element.on('blur', function (event) {
-      if (hasChanged) {
-        scope.$apply(function () {
-          fn(scope, {$event: event});
-        });
-        hasChanged = false;
-      }
-    });
-  };
-})
-
-.directive('onEnterBlur', function() {
-  return function(scope, element, attrs) {
-    element.bind("keydown keypress", function(event) {
-      if(event.which === 13) {
+}).directive('onBlurChange', [
+  '$parse',
+  function ($parse) {
+    return function (scope, element, attr) {
+      var fn = $parse(attr['onBlurChange']);
+      var hasChanged = false;
+      element.on('change', function (event) {
+        hasChanged = true;
+      });
+      element.on('blur', function (event) {
+        if (hasChanged) {
+          scope.$apply(function () {
+            fn(scope, { $event: event });
+          });
+          hasChanged = false;
+        }
+      });
+    };
+  }
+]).directive('onEnterBlur', function () {
+  return function (scope, element, attrs) {
+    element.bind('keydown keypress', function (event) {
+      if (event.which === 13) {
         element.blur();
         event.preventDefault();
       }
     });
   };
-})
-
+});
 ;
