bbb8b9932eb764d180d37390e160ff208e797b22
Thien Pham <thienpham.us@gmail.com>
2014-06-23 19:04:14 -0700

    fix contact.edit link, update ionic

--- build/src/app/app.js
+++ build/src/app/app.js
@@ -1,4 +1,4 @@
-angular.module('qpham', [
+  angular.module( 'qpham', [
   'ionic',
   'templates-app',
   'templates-common',
@@ -10,75 +10,76 @@ angular.module('qpham', [
   'qpham.services.projects',
   'qpham.directives',
   'firebase'
-]).config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  '$locationProvider',
-  function myAppConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider.otherwise('/projects');
-    $locationProvider.html5Mode(true).hashPrefix('!');
-  }
-]).run([
-  'FBURL',
-  '$rootScope',
-  '$location',
-  function run(FBURL, $rootScope, $location) {
+])
+
+/*.value('$anchorScroll', angular.noop)*/
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise( '/projects' );
+
+  $locationProvider.html5Mode(true).hashPrefix('!');
+})
+
+.run( function run (FBURL, $rootScope, $location) {
+  $rootScope.expand = false;
+  $rootScope.FBURL = FBURL;
+
+})
+
+// your Firebase URL goes here
+// should look something like: https://blahblahblah.firebaseio.com
+.constant('FBURL', 'https://qpham.firebaseio.com')
+
+.controller( 'AppCtrl', function AppCtrl ( $rootScope, $scope, $state, $stateParams, $window, $location, FBURL, projectsFactory) {
+  $scope.projects = projectsFactory.collection();
+  $scope.links = ['projects', 'about', 'resume', 'contacts'];
+
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+  $scope.$location = $location;
+
+  $scope.loadingObj = {};
+  $scope.lastSearch = {};
+  $scope.lastSearch.search = '';//saves last contacts search
+  $scope.loadingObj.loading = true; //shows loading spinner when true
+
+  $scope.isActive = function(project) {
+    if (project.$id && '/projects/' + project.$id == $location.path()) {
+      return true;
+    }
+
+    if ('/' + project == $location.path()) {
+      return true;
+    }
+
+    return false;
+  };
+
+  $scope.menuitemClick = function(project) {
+    if (project.$id) {
+      $location.path('/projects/' + project.$id);
+    } else if (project) {
+      $location.path('/' + project);
+    } else {
+      $location.path('/projects');
+    }
+    
     $rootScope.expand = false;
-    $rootScope.FBURL = FBURL;
-  }
-]).constant('FBURL', 'https://qpham.firebaseio.com').controller('AppCtrl', [
-  '$rootScope',
-  '$scope',
-  '$state',
-  '$stateParams',
-  '$window',
-  '$location',
-  'FBURL',
-  'projectsFactory',
-  function AppCtrl($rootScope, $scope, $state, $stateParams, $window, $location, FBURL, projectsFactory) {
-    $scope.projects = projectsFactory.collection();
-    $scope.links = [
-      'projects',
-      'about',
-      'resume',
-      'contacts'
-    ];
-    $rootScope.$state = $state;
-    $rootScope.$stateParams = $stateParams;
-    $scope.$location = $location;
-    $scope.loadingObj = {};
-    $scope.lastSearch = {};
-    $scope.lastSearch.search = '';
+  };
+
+  $scope.closeMenu = function() {
+    $rootScope.expand = false;
+  };
+
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $window.ga('send', 'pageview', {'page': $location.path()});
+  });
+
+  $scope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    $rootScope.expand = false;
     $scope.loadingObj.loading = true;
-    $scope.isActive = function (project) {
-      if (project.$id && '/projects/' + project.$id == $location.path()) {
-        return true;
-      }
-      if ('/' + project == $location.path()) {
-        return true;
-      }
-      return false;
-    };
-    $scope.menuitemClick = function (project) {
-      if (project.$id) {
-        $location.path('/projects/' + project.$id);
-      } else if (project) {
-        $location.path('/' + project);
-      } else {
-        $location.path('/projects');
-      }
-      $rootScope.expand = false;
-    };
-    $scope.closeMenu = function () {
-      $rootScope.expand = false;
-    };
-    $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-      $window.ga('send', 'pageview', { 'page': $location.path() });
-    });
-    $scope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-      $rootScope.expand = false;
-      $scope.loadingObj.loading = true;
-    });
-  }
-]);
+  });
+
+})
+
 ;
--- build/src/app/services/api.js
+++ build/src/app/services/api.js
@@ -1,21 +1,30 @@
-angular.module('qpham.services.api', []).factory('apiFactory', [
-  '$http',
-  function ($http) {
+angular.module('qpham.services.api', [])
+  .factory('apiFactory', function($http) {
     var urlBase = '/api';
     var contacts = $http.get(urlBase + '/contacts').then(function (resp) {
-        return resp.data;
-      });
+      return resp.data;
+    });
+
     var apiFactory = {};
-    apiFactory.getStates = function () {
+
+    apiFactory.getStates = function() { 
+      /*return Restangular.all('states').getList();*/
       return $http.get(urlBase + '/states');
     };
-    apiFactory.getContacts = function () {
+
+    apiFactory.getContacts = function() { 
+      /*return Restangular.all('contacts').getList();*/
+      /*return $http.get(urlBase + '/contacts');*/
       return contacts;
     };
-    apiFactory.getContact = function (id) {
+
+    apiFactory.getContact = function(id) { 
+      /*return Restangular.all('contacts').getList();*/
       return $http.get(urlBase + '/contacts/' + id);
     };
+
     return apiFactory;
-  }
-]);
-;
+
+  })
+
+  ;
